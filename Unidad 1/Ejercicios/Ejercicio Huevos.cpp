2100b0060458070fc0944e826a680e479feaa121
Guillermo Phelan <97048366+willyPhelan@users.noreply.github.com>
2025-03-30 16:40:27 -0300

    Update Ejercicio Huevos.cpp

--- Unidad 1/Ejercicios/Ejercicio Huevos.cpp
+++ Unidad 1/Ejercicios/Ejercicio Huevos.cpp
@@ -2,10 +2,10 @@
 Una granja vende la caja de 12 unidades a $1000 y cada huevo suelto a $120.
 Hacer un programa para ingresar la cantidad de huevos que compra un cliente y mostrar por pantalla el importe total a pagar.
 Recordatorio. Por cada 12 huevos se debe calcular una caja y el excedente se calcula como suelto.
-Ejemplo A. Si la cantidad ingresada son 15 huevos, el programa calcular· 1 caja y 3 sueltos, es decir $1000 + $120 x 3 = $1360.
-Ejemplo B. Si la cantidad ingresada son 28 huevos el programa calcular· 2 cajas y 4 sueltos, es decir $1000 x 2 + $120 x 4 = $2480.
-Ejemplo C. Si la cantidad ingresada son 8 huevos, el programa calcular·: 0 cajas y 8 sueltos, es decir $120 x 8 = $960.
-Ejemplo D. Si la cantidad ingresada son 200 huevos, el programa calcular·: 16 cajas y 8 sueltos, es decir $1000 x 16 + $120 x 8 = $16960. */
+Ejemplo A. Si la cantidad ingresada son 15 huevos, el programa calcular√° 1 caja y 3 sueltos, es decir $1000 + $120 x 3 = $1360.
+Ejemplo B. Si la cantidad ingresada son 28 huevos el programa calcular√° 2 cajas y 4 sueltos, es decir $1000 x 2 + $120 x 4 = $2480.
+Ejemplo C. Si la cantidad ingresada son 8 huevos, el programa calcular√°: 0 cajas y 8 sueltos, es decir $120 x 8 = $960.
+Ejemplo D. Si la cantidad ingresada son 200 huevos, el programa calcular√°: 16 cajas y 8 sueltos, es decir $1000 x 16 + $120 x 8 = $16960. */
 
 
 
@@ -29,7 +29,7 @@ int main() {
 	cant_cajas = huevos / 12 ;
 	resto_huevos = huevos % 12 ;
 
-	total_x_cajas = cant_cajas * 000 ;
+	total_x_cajas = cant_cajas * 1000 ;
 	total_x_hsueltos = resto_huevos * 120 ;
 
 	total = total_x_cajas + total_x_hsueltos ;
