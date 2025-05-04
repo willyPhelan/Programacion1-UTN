42dad400a1df746607bd5addb8bbe7f39bf45190
Guillermo Phelan <97048366+willyPhelan@users.noreply.github.com>
2025-05-03 23:07:01 -0300

    Add files via upload

---
+++ Unidad 3/Ejercicios/Ejercicio 19 partidos.cpp
@@ -0,0 +1,95 @@
+/*
+29	Se dispone de la información de los últimos 19 partidos del futbolista Diego Armando Laradona. Por cada partido se registró:
+-	Número de partido
+-	Minutos jugados
+-	Tarjetas amarillas
+-	Tarjetas rojas
+-	Goles
+
+Existe un registro para cada partido. Los mismos se encuentran ordenados por número de partido. Se pide calcular e informar:
+A)	La cantidad de partidos que no jugó (partidos con minutos igual a cero)
+B)	La cantidad de partidos que jugó por completo (minutos >= 90)
+C)	El promedio de tarjetas recibidas por partido.
+D)	El número de partido en el que haya convertido mayor cantidad de goles. Indicar también los goles convertidos.
+E)	La mejor racha de partidos convirtiendo goles. Se debe mostrar la mayor cantidad de partidos consecutivos en los que haya convertido.
+
+
+*/#include <iostream>
+
+
+using namespace std ;
+
+
+
+int main() {
+
+    int i, partidos, minutos_jugados, tarjetas_amarillas, tarjetas_rojas, goles, no_jugo=0, partido_completo=0, mejor_partido=0, tarjetas=0, total_partidos=0, mas_goles=0 ;
+    int mejor_racha=0, racha_actual=0 ;
+    float promedio=0 ;
+    bool hay_gol=false ;
+
+	for(i = 1 ; i <= 19 ; i++){
+
+        cout << "Ingrese el numero de partido:" << endl ;
+        cin >> partidos ;
+
+        cout << "Ingrese el numero de minutos jugados:" << endl ;
+        cin >> minutos_jugados ;
+
+        cout << "Ingrese el numero de tarjetas amarillas:" << endl ;
+        cin >> tarjetas_amarillas ;
+
+        cout << "Ingrese el numero de tarjetas rojas:" << endl ;
+        cin >> tarjetas_rojas ;
+
+        cout << "Ingrese el numero de goles:" << endl ;
+        cin >> goles ;
+
+        total_partidos++ ;
+
+        tarjetas += tarjetas_amarillas + tarjetas_rojas ;
+
+        if(minutos_jugados == 0) {
+            no_jugo++ ;
+        }
+
+        if(minutos_jugados >= 90){
+            partido_completo++ ;
+        }
+
+
+        if(hay_gol == false) {
+            if(goles != 0){
+                    mas_goles = goles ;
+                    mejor_partido = partidos ;
+                    hay_gol= true ;
+                    }}
+
+        if(goles > mas_goles){
+            mas_goles = goles ;
+            mejor_partido = partidos ; }
+
+        if(goles > 0){
+            racha_actual++ ;
+            if(racha_actual > mejor_racha){
+                mejor_racha = racha_actual ;
+            } }else { racha_actual = 0 ;} // se corta la racha
+
+
+            }
+
+        promedio = tarjetas / total_partidos ;
+
+        if(no_jugo > 0){
+        cout << endl << "No jugo " << no_jugo << " partido/s" << endl ;}
+
+     cout << "Jugo " << partido_completo << " partido/s completo/s" << endl ;
+     cout << "Tarjetas totales: " << tarjetas << endl ;
+     cout << "Promedio de tarjetas por partido: " << promedio << endl ;
+     cout << "Numero de partido donde mas convirtio: " << mejor_partido << " Goles: " << mas_goles << endl ;
+     cout << "Mejor racha de partidos convirtiendo goles: " << mejor_racha << " partidos consecutivos" << endl ;
+
+
+
+	return 0 ;
+ }
---
+++ Unidad 3/Ejercicios/Ejercicio Cuenta Corriente.cpp
@@ -0,0 +1,92 @@
+/*
+28	La cuenta corriente de la famosa cantante Lady Lara ha registrado 14 movimientos durante la semana pasada. Por cada movimiento se registró:
+-	Número de movimiento
+-	Día
+-	Tipo ('E' - Extracción / 'D' - Depósito)
+-	Importe
+
+Existe un registro por movimiento. Se desea calcular e informar:
+-	El saldo final de la cuenta.
+-	El porcentaje de movimientos de extracción y el porcentaje de depósito.
+-	El depósito de mayor importe indicando también día y número de movimiento.
+-	La cantidad de movimientos del día 10.
+
+*/#include <iostream>
+
+
+using namespace std ;
+
+
+
+int main() {
+
+    int i, numero_movimiento, movimiento=0, dia, depo=0, extra=0, depositos, mov_mayor=0, dia_mayor=0, mov_dia_10=0 ;
+    char tipo ;
+    float importe, total=0, porcentaje_d=0, porcentaje_e=0, depo_mayor=0 ;
+    bool hay_depo=false ;
+
+	for(i = 1 ; i <= 14 ; i++){
+
+         movimiento++ ;
+
+         cout << "Ingrese el numero de movimiento" << endl ;
+         cin >> numero_movimiento ;
+
+         cout << "Ingrese el dia que realizo el movimiento" << endl ;
+         cin >> dia ;
+
+         cout << "Indique el tipo de movimiento: E para extraccion o D para deposito" << endl  ;
+         cin >> tipo ;
+
+         cout << "Indique el  importe del movimiento realizado" << endl ;
+         cin >> importe ;
+
+
+         if(tipo == 'e') {
+            total -= importe ;
+            extra++ ;
+             } else {
+                 total += importe ;
+                 depo++ ; }
+
+
+        porcentaje_d = (depo * 100) / 4 ;
+        porcentaje_e = (extra * 100) / 4 ;
+
+        if(tipo == 'd' && i == 1) {
+           hay_depo = true ;
+           depo_mayor = importe ;
+           mov_mayor = numero_movimiento ;
+           dia_mayor = dia ; }
+
+        if(tipo == 'd' && hay_depo == true) {
+                if(importe > depo_mayor){
+                depo_mayor = importe ;
+                mov_mayor = numero_movimiento ;
+                dia_mayor = dia ;}}
+
+        if(dia == 10){
+                mov_dia_10++ ;
+        }
+
+	} // -- for
+
+	if(total < importe) {
+        cout << endl << "Saldo insuficiente para realizar la operacion" << endl ;
+	} else {
+
+	    cout << endl << "Porcentaje de depositos: %" << porcentaje_d << endl ;
+        cout << "Porcentaje de extacciones: %" << porcentaje_e << endl ;
+        cout << "Deposito de mayor importe: $" << depo_mayor << " Nro. de movimiento: " << mov_mayor << " El dia: " << dia_mayor <<endl ;
+        cout << "Movimientos del dia 10: " << mov_dia_10 << endl ;
+        cout << "Saldo final: $" << total << endl ;
+        }
+
+
+
+
+
+
+
+	return 0 ;
+ }
