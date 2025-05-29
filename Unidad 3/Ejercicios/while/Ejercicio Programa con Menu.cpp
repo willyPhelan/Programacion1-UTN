3a3edcd9e3d6a108620675d30aae00d7b4aa27fd
Guillermo Phelan <97048366+willyPhelan@users.noreply.github.com>
2025-05-29 13:28:32 -0300

    Add files via upload

---
+++ Unidad 3/Ejercicios/while/Ejercicio Programa con Menu.cpp
@@ -0,0 +1,127 @@
+/*
+Hacer un programa que contenga un menú con el siguiente formato:
+
+Menú principal
+-----------------------------
+1 - Ingresar medida en metros
+2 - Convertir a centímetros
+3 - Convertir a kilómetros
+4 - Convertir a pulgadas
+5 - Convertir a pies
+-----------------------------
+0 - Salir del programa
+
+Ingrese opción:
+
+Al ingresar a la opción 1, nos pedirá una medida (float) en metros que con las siguientes opciones del menú podremos mostrarla convertida a otras unidades.
+El programa debe permitir al usuario ingresar a las opciones las veces que lo desee y sólo finalizará al ingresar a la opción 0.
+
+
+*/
+
+#include <iostream>
+
+
+using namespace std ;
+
+
+
+int main() {
+
+    int opcion=-1 ;
+    float metros ;
+    bool metrosIngresados = false ;
+
+    cout << "------------------- MENU -------------------------" << endl ;
+
+    while(opcion != 0){
+
+
+            cout << "--------------------------------------------------" << endl ;
+
+            cout << "Ingrese la opcion [1] para ingresar los metros" << endl ;
+
+            cout << "--------------------------------------------------" << endl ;
+
+            cout << "Ingrese la opcion [2] para convertir a centimetros" << endl ;
+
+            cout << "--------------------------------------------------" << endl ;
+
+            cout << "Ingrese la opcion [3] para convertir a kilometros" << endl ;
+
+            cout << "--------------------------------------------------" << endl ;
+
+            cout << "Ingrese la opcion [4] para convertir a pulgadas" << endl ;
+
+            cout << "--------------------------------------------------" << endl ;
+
+            cout << "Ingrese la opcion [5] para convertir a pies" << endl ;
+
+            cout << "--------------------------------------------------" << endl ;
+            cout << "--------------------------------------------------" << endl ;
+
+            cout << "Ingrese la opcion [0] para finalizar" << endl << endl ;
+
+            cin >> opcion ;
+
+            cout << "--------------------------------------------------" << endl ;
+
+
+             switch (opcion) {
+
+            case 1:
+                cout << "Ingrese la medida en metros: " ;
+                cin >> metros ;
+                metrosIngresados = true ;
+                break ;
+
+            case 2:
+                if (metrosIngresados)
+                    cout << metros << " metros son " << metros * 100 << " centimetros." << endl ;
+                else
+                    cout << "Primero debe ingresar una medida en metros (opcion 1)." << endl ;
+                break ;
+
+            case 3:
+                if (metrosIngresados)
+                    cout << metros << " metros son " << metros / 1000 << " kilometros." << endl ;
+                else
+                    cout << "Primero debe ingresar una medida en metros (opcion 1)." << endl ;
+                break ;
+
+            case 4:
+                if (metrosIngresados)
+                    cout << metros << " metros son " << metros * 39.3701 << " pulgadas." << endl ;
+                else
+                    cout << "Primero debe ingresar una medida en metros (opcion 1)." << endl ;
+                break ;
+
+            case 5:
+                if (metrosIngresados)
+                    cout << metros << " metros son " << metros * 3.28084 << " pies." << endl ;
+                else
+                    cout << "Primero debe ingresar una medida en metros (opcion 1)." << endl ;
+                break ;
+
+            case 0:
+                cout << "Programa finalizado!" << endl;
+                break;
+            default:
+                cout << "Opción inválida. Intente nuevamente." << endl;
+                break;
+        }
+
+
+            cout << "--------------------------------------------------" << endl ;
+            cout << "--------------------------------------------------" << endl ;
+
+
+
+
+
+             }
+
+
+
+	return 0 ;
+ }
---
+++ Unidad 3/Ejercicios/while/Ejercicio Universidad.cpp
@@ -0,0 +1,39 @@
+/*
+Una Universidad dispone de diez aulas para acomodar a una cantidad de asistentes a una charla. Cada aula tiene una capacidad total de 60 personas.
+Se necesita un programa que solicite la cantidad de asistentes a la charla y  determine la cantidad total de aulas necesarias para acomodarlos a todos.
+
+Por ejemplo:
+Si los asistentes son 120 se necesitarán 2 aulas.
+Si los asistentes son 123 se necesitarán 3 aulas.
+Si los asistentes son 40 se necesitará 1 aula.
+
+DESAFÍO: No utilizar el operador de división ni el de resto en el algoritmo.
+
+
+*/
+
+#include <iostream>
+
+
+using namespace std ;
+
+
+
+int main() {
+
+    int num, aula=1 ;
+
+	cout << "Ingrese el numero de asistentes a la charla" << endl ;
+	cin >> num ;
+
+	if(num > 60){
+        aula++ ;
+	}
+
+	if (num > 120){
+            aula++ ;}
+
+	cout << "Se necesitan: " << aula << " aula/s" << endl ;
+
+	return 0 ;
+ }
